2eb2030de68ca83100da3678f26e4d9cb5b218c6
Leticia <contatoleticiadia@gmail.com>
2021-10-16 14:10:26 -0300

    feat: add lookingAllStudents() method

--- commands/student-search.php
+++ commands/student-search.php
@@ -8,12 +8,10 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 $entityManagerFactory = new EntityManagerFactory();
 $entityManager = $entityManagerFactory->getEntityManager();
+$studentRepository = $entityManager->getRepository(Student::class);
 
-$classStudent = Student::class;
-$dql = "SELECT student FROM $classStudent student WHERE student.id=1 OR student.name='Gabriela Morais'";
-$query = $entityManager->createQuery($dql);
 /** @var Student[] $studentList */
-$studentList = $query->getResult();
+$studentList = $studentRepository->lookingAllStudents();
 
 foreach ($studentList as $student) {
     $phones = $student->getPhones()->map(function(Phone $phone){
